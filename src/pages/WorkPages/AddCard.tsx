5a08f9ab7375f09bb35b775467e5f698554ccc88
Aytaj Mammadova <124623586+aitajmammadova@users.noreply.github.com>
2024-11-19 18:47:08 +0000

    paymenttt

--- src/pages/WorkPages/AddCard.tsx
+++ src/pages/WorkPages/AddCard.tsx
@@ -1,19 +1,35 @@
-// import InputCompany from "../../components/component/Company/InputCompany";
-
+import InputCompany from "../../components/component/Company/InputCompany";
 // import toast from "react-hot-toast";
-
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { formSchemaProfileUpdate } from "../../lib/schemas";
+import Select from "react-select";
+import { Label } from "../../components/ui/label";
 import { useState } from "react";
 import { Button } from "../../components/ui/button";
-// import {
-//     Form,
-//     FormControl,
-//     FormField,
-//     FormItem,
-//     FormMessage,
-// } from "../../components/ui/form";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormMessage,
+} from "../../components/ui/form";
 
 export default function AddCard() {
     const [firstPart, setFirstPart] = useState(true)
+    const form = useForm<z.infer<typeof formSchemaProfileUpdate>>({
+        resolver: zodResolver(formSchemaProfileUpdate),
+        defaultValues: {
+            firstname: "",
+            lastname: "",
+            website: "",
+            bio: "",
+            username: "",
+            city: "",
+
+        },
+    });
 
     return (
         <div className="text-white flex-1 flex flex-col overflow-hidden relative">
@@ -32,19 +48,177 @@ export default function AddCard() {
             </section>
             <div className="bg-[url(/assets/images/bg-hacktivity.png)] bg-center bg-no-repeat bg-cover flex-1 lg:px-20 sm:px-8 px-3  py-16">
                 {firstPart ? (
-                    <div>First</div>
+
+
+                    <div className="mt-4">
+                        <h2 className="sm:text-[23px] text-[16px] font-[600] mb-8 ">
+                            PAYMENT INFORMATION
+                        </h2>
+                        <div className="main">
+                            <Form {...form}>
+                                <form
+                                    //onSubmit={form.handleSubmit(onSubmit)}
+                                    className="space-y-4 sm:space-y-8">
+                                    <div className="flex sm:items-center gap-4 sm:flex-row">
+                                        <div className="">
+                                            <Label className="sm:text-[18px] text-[14px] font-[600] md:min-w-[130px] sm:min-w-[100px] ">
+                                                First Name
+                                            </Label>
+                                            <FormField
+                                                //control={form.control}
+                                                name="firstname"
+
+                                                render={({ field }) => (
+                                                    <FormItem>
+                                                        <FormControl>
+                                                            <input
+                                                                type="text"
+
+                                                                {...field}
+                                                                //defaultValue={userDate?.first_name || ''}
+                                                                className="bg-[rgba(254,60,183,0.2)] border-[3px] border-solid border-[rgba(255,255,255,0.13)] text-white rounded-[25px] focus:outline-none focus-visible:ring-0 focus-visible:ring-offset-1 placeholder:text-white py-6 h-[50px] mt-[20px] xl:min-w-[250px]"
+                                                            />
+                                                        </FormControl>
+                                                        <FormMessage />
+                                                    </FormItem>
+                                                )}
+                                            />
+
+                                        </div>
+                                        <div className="">
+                                            <Label className="sm:text-[18px] text-[14px] font-[600] md:min-w-[130px] sm:min-w-[100px] ">
+                                                Last Name
+                                            </Label>
+                                            <FormField
+                                                //control={form.control}
+                                                name="firstname"
+
+                                                render={({ field }) => (
+                                                    <FormItem>
+                                                        <FormControl>
+                                                            <input
+                                                                type="text"
+
+                                                                {...field}
+                                                                //defaultValue={userDate?.first_name || ''}
+                                                                className="bg-[rgba(254,60,183,0.2)] border-[3px] border-solid border-[rgba(255,255,255,0.13)] text-white rounded-[25px] focus:outline-none focus-visible:ring-0 focus-visible:ring-offset-1 placeholder:text-white py-6 h-[50px] mt-[20px] xl:min-w-[250px]"
+                                                            />
+
+                                                        </FormControl>
+                                                        <FormMessage />
+                                                    </FormItem>
+                                                )}
+                                            />
+
+                                        </div>
+                                    </div>
+
+                                    <div className="flex sm:items-center gap-4 sm:flex-row">
+                                        <div className="">
+                                            <Label className="sm:text-[18px] text-[14px] font-[600] md:min-w-[130px] sm:min-w-[100px] ">Country
+                                            </Label>
+                                            <FormField
+                                                //control={form.control}
+                                                name="country"
+
+                                                render={({ field }) => (
+                                                    <FormItem>
+                                                        <FormControl>
+                                                            <input
+                                                                type="text"
+
+                                                                {...field}
+                                                                //defaultValue={userDate?.first_name || ''}
+                                                                className="bg-[rgba(254,60,183,0.2)] border-[3px] border-solid border-[rgba(255,255,255,0.13)] text-white rounded-[25px] focus:outline-none focus-visible:ring-0 focus-visible:ring-offset-1 placeholder:text-white py-6 h-[50px] mt-[20px] xl:min-w-[250px]"
+                                                            />
+                                                        </FormControl>
+                                                        <FormMessage />
+                                                    </FormItem>
+                                                )}
+                                            />
+
+                                        </div>
+                                        <div className="">
+                                            <Label className="sm:text-[18px] text-[14px] font-[600] md:min-w-[130px] sm:min-w-[100px] ">City</Label>
+                                            <FormField
+                                                //control={form.control}
+                                                name="city"
+
+                                                render={({ field }) => (
+                                                    <FormItem>
+                                                        <FormControl>
+                                                            <input
+                                                                type="text"
+
+                                                                {...field}
+                                                                //defaultValue={userDate?.first_name || ''}
+                                                                className="bg-[rgba(254,60,183,0.2)] border-[3px] border-solid border-[rgba(255,255,255,0.13)] text-white rounded-[25px] focus:outline-none focus-visible:ring-0 focus-visible:ring-offset-1 placeholder:text-white py-6 h-[50px] mt-[20px] xl:min-w-[250px]"
+                                                            />
+
+                                                        </FormControl>
+                                                        <FormMessage />
+                                                    </FormItem>
+                                                )}
+                                            />
+
+                                        </div>
+                                        <div className="">
+                                            <Label className="sm:text-[18px] text-[14px] font-[600] md:min-w-[130px] sm:min-w-[100px] ">Address
+                                            </Label>
+                                            <FormField
+                                                //control={form.control}
+                                                name="address"
+
+                                                render={({ field }) => (
+                                                    <FormItem>
+                                                        <FormControl>
+                                                            <input
+                                                                type="text"
+
+                                                                {...field}
+                                                                //defaultValue={userDate?.first_name || ''}
+                                                                className="bg-[rgba(254,60,183,0.2)] border-[3px] border-solid border-[rgba(255,255,255,0.13)] text-white rounded-[25px] focus:outline-none focus-visible:ring-0 focus-visible:ring-offset-1 placeholder:text-white py-6 h-[50px] mt-[20px] xl:min-w-[250px]"
+                                                            />
+
+                                                        </FormControl>
+                                                        <FormMessage />
+                                                    </FormItem>
+                                                )}
+                                            />
+
+                                        </div>
+                                    </div>
+
+
+
+
+
+
+                                    <div className="sm:space-x-8 flex items-center mt-16 justify-end  flex-col sm:flex-row gap-4 sm:gap-0">
+                                        <Button className="hover:scale-110 transition-all duration-300 rounded-xl h-[45px] text-black sm:h-[50px] w-full sm:w-[220px] bg-[#FFDE31]  sm:text-[18px] font-[600] text-[16px]   hover:bg-[#FFDE31]">
+                                            Add
+                                        </Button>
+                                    </div>
+                                </form>
+                            </Form>
+                        </div>
+                    </div>
+
+
+
+
                 ) : (
                     <div>Second</div>
                 )}
 
                 <div className="w-[600] flex  justify-center content-center">
-                    
+
                     <Button className="hover:scale-105 transition-all duration-300 rounded-3xl  py-[7px]  bg-transparent text-white  border-2 border-[rgba(255,234,0,0.3)]  font-[600] hover:bg-[rgba(255,234,0,0.3)] flex gap-4 px-4 w-[160px]" onClick={() => setFirstPart(true)}>BACK
                     </Button>
                     <Button className="hover:scale-105 transition-all duration-300 rounded-3xl  py-[7px]  bg-transparent text-white  border-2 border-[rgba(255, 255, 255, 0.1) 50%)] bg-[rgba(255,234,0,0.3)] font-[600] hover:bg-transparent flex gap-4 px-4 w-[160px] ml-[10px]" onClick={() => setFirstPart(false)}>NEXT
                     </Button>
                 </div>
-                
+
             </div>
         </div>
     );
--- src/pages/WorkPages/Payment.tsx
+++ src/pages/WorkPages/Payment.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "../../components/ui/button";
 import { useNavigate } from "react-router-dom";
+
 export default function Payment() {
     const navigate = useNavigate();
     
